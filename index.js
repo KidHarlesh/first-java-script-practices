714f899d0e6aaec4e6cb1d51b2593d056302e9e9
KidHarlesh <alowoesintoluwalase@gmail.com>
2024-12-05 07:18:16 -0800

    understanding function

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-var passwordInput = document.getElementById("passme");
-var passwordIcon = document.getElementById("icone");
-var confirmPassword = document.getElementById("passwe");
-var iconPassword = document.getElementById("iconf");
-function confirm(input, icon) {
-  if (input.type === "password") {
-    input.type = "text";
-    icon.classList.remove("fa-eye-slash");
-    icon.classList.add("fa-eye");
+// var passwordInput = document.getElementById("passme");
+// var passwordIcon = document.getElementById("icone");
+// var confirmPassword = document.getElementById("passwe");
+// var iconPassword = document.getElementById("iconf");
+// function confirm(input, icon) {
+//   if (input.type === "password") {
+//     input.type = "text";
+//     icon.classList.remove("fa-eye-slash");
+//     icon.classList.add("fa-eye");
+//   } else {
+//     input.type = "password";
+//     icon.classList.remove("fa-eye");
+//     icon.classList.add("fa-eye-slash");
+//   }
+// }
+
+function confirm() {
+  var passwordInput = document.getElementById("passme");
+  var passwordIcon = document.getElementById("icone");
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    passwordIcon.classList.remove("fa-eye-slash");
+    passwordIcon.classList.add("fa-eye");
   } else {
-    input.type = "password";
-    icon.classList.remove("fa-eye");
-    icon.classList.add("fa-eye-slash");
+    passwordInput.type = "password";
+    passwordIcon.classList.remove("fa-eye");
+    passwordIcon.classList.add("fa-eye-slash");
+    
   }
+}
+function passme() {
+ var confirmPassword = document.getElementById("passwe");
+ var iconPassword = document.getElementById("iconf");
+ if (confirmPassword.type=== "password"){
+  confirmPassword.type = "text";
+    iconPassword.classList.remove("fa-eye-slash");
+    iconPassword.classList.add("fa-eye");
+ }else{
+  confirmPassword.type = "password";
+  iconPassword.classList.remove("fa-eye");
+  iconPassword.classList.add("fa-eye-slash");
+ }
 }
